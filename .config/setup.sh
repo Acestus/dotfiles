993640debcd40779c6b797983e2ffc1da34782d0
William. W.B. <36773237+Acestus@users.noreply.github.com>
2025-05-03 23:19:07 -0500

    Delete .config directory

--- .config/setup.sh
+++
@@ -1,5 +0,0 @@
-echo "Setup test"
-
-mkdir /home/vscode/.config/powershell
-
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.config/powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
