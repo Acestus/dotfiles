a5dc8bf29051cabb475130bec1235d8c113dc4be
William. W.B. <36773237+Acestus@users.noreply.github.com>
2025-05-03 22:24:42 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "Startup test"
+
+mkdir "/home/vscode/.config/powershell"
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.config/powershell/Microsoft.Powershell_profile.ps1 /home/vscode/.config/powershell/Microsoft.PowerShell_profile.ps1
