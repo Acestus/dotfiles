49fac564254ad9080f3e1adc276649c09317ef23
William. W.B. <36773237+Acestus@users.noreply.github.com>
2025-05-03 21:55:58 -0500

    Update setup.sh

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,3 +1,5 @@
 echo "Setup test"
 
+mkdir ~/.config/powershell
+
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.config/powershell/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
